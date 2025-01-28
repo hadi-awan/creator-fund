3b4ae960f69ea74033898413bc7887178761d4ed
hadi-awan <87779346+hadi-awan@users.noreply.github.com>
2025-01-27 22:57:21 -0500

    Added user role and user status enums

---
+++ backend/creatorfund/src/main/java/com/creatorfund/model/UserRole.java
@@ -0,0 +1,5 @@
+package com.creatorfund.model;
+
+public enum UserRole {
+    CREATOR, BACKER, ADMIN
+}
---
+++ backend/creatorfund/src/main/java/com/creatorfund/model/UserStatus.java
@@ -0,0 +1,5 @@
+package com.creatorfund.model;
+
+public enum UserStatus {
+    ACTIVE, INACTIVE, SUSPENDED;
+}
